4ef3dd32ce609be6eb94afffb118a6ee969d533e
Moses Samuel <mosesfemi10@gmail.com>
2026-07-05 13:55:20 +0100

    add: route with static file

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 
 //Define the application Environment
 const NODE_ENV = process.env.NODE_ENV?.toLocaleLowerCase || 'production';
 
 //Define the PORT number the server will listen On
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 
@@ -22,4 +28,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on a locallhost:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
 });
+
 
